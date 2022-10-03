7ec86def1d3e22c975c03d5c7c3feda3c0dcbaa2
bhunt222 <bhunt2018@gmail.com>
2022-10-03 15:03:29 -0400

    Boilerplate

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include 'includes/head.php'; ?>
+</head>
+<body>
+
+</body>
+<footer>
+    <?php include 'includes/foot.php'; ?>
+</footer>
+</html>
