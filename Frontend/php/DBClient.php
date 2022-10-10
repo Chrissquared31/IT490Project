64a5372165118c32790d3c8e1bd6436092f7bf03
az268 <az268@njit.edu>
2022-10-10 14:19:56 -0400

    fixed paths

--- Frontend/php/DBClient.php
+++ Frontend/php/DBClient.php
@@ -1,10 +1,16 @@
 #!/usr/bin/php
 <?php
-require_once('../rabbitmqphp_example/path.inc');
-require_once('../rabbitmqphp_example/get_host_info.inc');
-require_once('../rabbitmqphp_example/rabbitMQLib.inc');
+require_once('../../rabbitmqphp_example/path.inc');
+require_once('../../rabbitmqphp_example/get_host_info.inc');
+require_once('../../rabbitmqphp_example/rabbitMQLib.inc');
 
 $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+if(isset($argv[1])) {
+	$msg = $argv[1];
+}
+else {
+	$msg = "test message";
+}
 
 
 // REGISTRATION 
@@ -20,8 +26,4 @@ if (isset($_POST['register'])) {
 	echo($response);
 }
 
-
-
-
-?>
 
