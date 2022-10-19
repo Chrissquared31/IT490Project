b61c401055a13448c9cf67ed6e0c5b449a8d5745
chris <cnc9@njit.edu>
2022-10-19 12:48:31 -0400

    message

--- Frontend/php/DBClient.php
+++ Frontend/php/DBClient.php
@@ -5,45 +5,22 @@ require_once('../../rabbitmqphp_example/get_host_info.inc');
 require_once('../../rabbitmqphp_example/rabbitMQLib.inc');
 
 $client = new rabbitMQClient("../../rabbitmqphp_example/testRabbitMQ.ini","testServer");
-if(isset($argv[1])) {
-	$msg = $argv[1];
-}
-else {
-	$msg = "test message";
-}
+
+$request = array();
+$request['type'] = "registerUser";
+$request['username'] = $_POST["username"];
+$request['email'] = $_POST["email"];
+$request['password'] = $_POST["password"];
+$response = $client->send_request($request);
+
+echo "client received response: ".PHP_EOL;
+print_r($response);
+echo "\n\n";
+
+echo $argv[0]." END".PHP_EOL;
 
 
-// REGISTRATION 
 
-if (isset($_POST['register'])) {
-	$client = new rabbitMQClient("../../rabbitmqphp_example/testRabbitMQ.ini","testServer");
-	$request = array();
-	$request['type'] = "registerUser";
-	$request['username'] = $_POST["username"];
-	$request['email'] = $_POST["email"];
-	$request['password'] = $_POST["password"];
-	$response = $client->send_request($request);
 
-	echo "client received response: ".PHP_EOL;
-	print_r($response);
-	echo "\n\n";
 
-	echo $argv[0]." END".PHP_EOL;
-}
-
-//USER LOGIN
-
-if (isset($_POST['login'])) {
-	$request = array();
-	$request['type'] = "Login";
-	$request['username'] = $_POST["username"];
-	$request['password'] = $_POST["password"];
-	$response = $client->sent_request($request);
-
-	echo "client received response: ".PHP_EOL;
-	print_r($response);
-	echo "\n\n";
-
-	echo $argv[0]." END".PHP_EOL;
-}
 
